b12240bfc1f55ecdf77ff5c796dc024301b307f3
James Aduke <jamesodwuor4@gmail.com>
2022-02-21 16:02:11 +0300

    add popular products section

---
+++ src/components/Product.jsx
@@ -0,0 +1,84 @@
+import { FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined } from "@material-ui/icons";
+import styled from "styled-components";
+
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5s ease;
+`;
+
+const Container = styled.div`
+  flex: 1;
+  margin:5px;
+  min-width: 280px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  position: relative;
+  &:hover ${Info} {
+    opacity: 1;
+  }
+`;
+
+// const Circle = styled.div`
+//   width: 200px;
+//   height: 200px;
+//   border-radius: 50%;
+//   background-color: #fff;
+// `;
+const Image = styled.img`
+  height: 75%;
+
+`;
+
+const Icon = styled.div`
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background-color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 5px;
+  cursor: pointer;
+
+  &:hover{
+    background-color: #e9f5f5;
+    transform: scale(1.1);
+  }
+`;
+
+
+const Product = ({item}) => {
+  return (
+    <Container>
+      {/* <Circle /> */}
+      <Image src={item.img} />
+
+      <Info>
+        <Icon>
+          <ShoppingCartOutlined />
+        </Icon>
+
+        <Icon>
+          <SearchOutlined />
+        </Icon>
+
+        <Icon>
+          <FavoriteBorderOutlined />
+        </Icon>
+      </Info>
+    </Container>
+  )
+}
+
+export default Product
--- src/data.js
+++ src/data.js
@@ -14,7 +14,7 @@ export const sliderItems = [
         bg: "fcf1ed",
     },
     {
-        id: 1,
+        id: 3,
         img:"https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
         title: "POPULAR  SALE!!",
         description: "DON'T COMPROMISE GET THE BEST PRICE",
@@ -39,4 +39,54 @@ export const categories = [
         img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
         title: "LIGHT JACKETS",
     },
-]
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS339/10P4F0192/UCS339_10P4_F0192_S_211_SLF.png/jcr:content/renditions/cq5dam.web.hebebed.1000.1000.square.jpg",
+    },
+
+    {
+        id: 2,
+        img: "https://www.prada.com/content/dam/pradanux_products/S/SC5/SC567/K2GF0002/SC567_K2G_F0002_S_221_SLF.png/jcr:content/renditions/cq5dam.web.hebebed.1000.1000.square.jpg",
+    },
+    {
+        id: 3,
+        img: "https://www.prada.com/content/dam/pradanux_products/S/SC4/SC449/1WQ8F0009/SC449_1WQ8_F0009_S_182_SLF.png/jcr:content/renditions/cq5dam.web.hebebed.1000.1000.square.jpg",
+    },
+    {
+        id: 4,
+        img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/113/BUS-PAT-BURTE-1210819/1170x1470_BS_2019_08_121_back_1562587855460.jpg",
+    },
+
+    {
+        id: 5,
+        img:"https://www.burdastyle.com/pub/media/catalog/product/cache/ce2577252a9a02a83a7a26f75697304a/288/BUS-PAT-BURTE-1251221/BS_2021_12_125_front.jpg",
+    },
+
+    {
+        id: 6,
+        img:"https://www.burdastyle.com/pub/media/catalog/product/cache/ce2577252a9a02a83a7a26f75697304a/285/BUS-PAT-BURTE-1281121/bs_2021_11_128_front.jpg",
+    },
+
+    {
+        id: 7,
+        img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/285/BUS-PAT-BURTE-1261121/bs_2021_11_126_front.jpg",
+    },
+    {
+        id: 8,
+        img: "https://www.burdastyle.com/pub/media/catalog/product/cache/ce2577252a9a02a83a7a26f75697304a/113/BUS-PAT-BURTE-1280719/1170x1470_BS_2019_07_128_front_1559812096968.jpg"
+    },
+     {
+        id: 9,
+        img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1331216/1170x1470_BS_2016_12_133_front.png"
+     },
+
+     {
+        id: 10,
+         img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/106/BUS-PAT-BURTE-1210517/1170x1470_BS_2017_05_121_front.png"
+     }
+
+
+];
