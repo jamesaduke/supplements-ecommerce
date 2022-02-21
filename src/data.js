7805f04d2d07d90ea88fd8cf263932889417ef13
James Aduke <jamesodwuor4@gmail.com>
2022-02-21 13:57:39 +0300

    add categories ui

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const sliderItems = [
     {
         id: 1,
-        img:"https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        img:"https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60gr",
         title: "VALENTINE SALE!!",
         description: "DON'T COMPROMISE GET THE BEST PRICE",
         bg: "f5fafd",
@@ -21,3 +21,22 @@ export const sliderItems = [
         bg: "fbf0f4",
     },
 ];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        title: "SHIRTS",
+
+    },
+    {
+        id: 2,
+        img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        title: "LOUNGEWEAR",
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        title: "LIGHT JACKETS",
+    },
+]
