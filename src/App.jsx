b47b70d4ae3fa59fa7a2a6fa08db0567a8cb943c
James Aduke <jamesodwuor4@gmail.com>
2022-02-24 15:19:15 +0300

    register markup and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Home from "./Pages/Home";
 import ProductList from "./Pages/ProductList";
 import Product from "./Pages/Product";
+import Register from "./Pages/Register";
 
 const App = () => {
   return (
-    <Product />
+    <Register />
   )
 };
 
