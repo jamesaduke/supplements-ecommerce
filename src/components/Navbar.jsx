7f807a5b4e991bb85e2c6e8bc3f7c3fa28325610
james <jamesodwuor4@gmail.com>
2022-02-25 12:21:56 +0300

    responsiveness for Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,30 +2,40 @@ import React from 'react'
 import styled from 'styled-components'
 import {Search, ShoppingCartOutlined} from "@material-ui/icons";
 import {Badge} from "@material-ui/core";
-
+import { mobile } from  "../responsive";
 
 const Container = styled.div`
-    height: 60px;
-
+  height: 60px;
+  ${mobile({
+    height: "50px",
+    width: "99vw"
+  })}
 `;
 const Wrapper = styled.div`
     padding: 10px 20px;
     display: flex;
     align-items: center;
     justify-content: space-between;
+  ${mobile({
+    padding: "10px 0px"
+  })}
 
 `;
 
 const Input = styled.input`
     width: 100%;
-    height: 40px;
+    //height: 40px;
     border: none;
-    padding: 0 15px;
-    font-size: 14px;
-    outline: none;
-    &:focus {
-        border: 1px solid #00bcd4;
-    }
+    //padding: 0 15px;
+    //font-size: 14px;
+    //outline: none;
+    //&:focus {
+    //    border: 1px solid #00bcd4;
+    //}
+    ${mobile({
+      width: "100%"
+      
+    })}
   `;
 
 const Left = styled.div`
@@ -39,6 +49,13 @@ const SearchContainer = styled.div`
     width: 60%;
     display: flex;
     align-items: center;
+    ${mobile
+        (
+            {
+                marginLeft: "10px"
+            }
+        )
+    }
 `
 
 const Center = styled.div`
@@ -48,20 +65,43 @@ const Center = styled.div`
 
 const Logo = styled.div`
     font-weight: bold;
+    ${mobile
+      (
+          {
+            fontSize: "14px"
+          }
+      )
+    }
   
-`
+`;
 
 const Right = styled.div`
     flex: 1;
     display: flex;
     align-items: center;
     justify-content: flex-end;
+    ${mobile
+      (
+              {
+                justifyContent: "center",
+                flex: 2
+              }
+      )
+    }
 `;
 const MenuItem = styled.div`
   font-size: 14px;
   font-weight: 300;
   cursor: pointer;
   margin-left: 20px;
+  ${mobile
+      (
+              {
+                fontSize: "12px",
+                marginLeft: "10px"
+              }
+      )
+  }
 `
 
 const Navbar = () => {
@@ -71,7 +111,7 @@ const Navbar = () => {
             <Left>
                 <SearchContainer>
                     <Input placeholder="Search"/>
-                    <Search style={{ color: "gray", fontSize: 20}}/>
+                    <Search style={{ color: "gray", fontSize: "15px"}}/>
                 </SearchContainer>
             </Left>
             <Center>
